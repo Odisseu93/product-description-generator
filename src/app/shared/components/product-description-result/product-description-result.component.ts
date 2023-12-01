114f6b71438896adf0acf7dae40e2da67e40b044
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2023-12-01 20:13:10 -0300

    --amend

--- src/app/shared/components/product-description-result/product-description-result.component.ts
+++ src/app/shared/components/product-description-result/product-description-result.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { Product } from '../form-description-product/types/product.type'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-product-description-result',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-description-result.component.html',
   styleUrl: './product-description-result.component.scss',
 })
-export class ProductDescriptionResultComponent {}
+export class ProductDescriptionResultComponent {
+  @Input() product: Product | undefined
+}
