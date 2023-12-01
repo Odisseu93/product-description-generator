2b509807264b46585b697f8f446681c8c2879af7
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2023-12-01 20:12:09 -0300

    feat: add form validations and new visual elements in the form UI

--- src/app/shared/components/form-description-product/form-product-description.component.ts
+++ src/app/shared/components/form-description-product/form-product-description.component.ts
@@ -1,27 +1,61 @@
 import { Component } from '@angular/core'
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms'
-import { ProductDescriptionResultComponent } from '../product-description-result/product-description-result.component'
-import { Product } from './types/product.type'
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-form-product-description',
   standalone: true,
-  imports: [ReactiveFormsModule, ProductDescriptionResultComponent],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-product-description.component.html',
   styleUrl: './form-product-description.component.scss',
 })
 export class FormProductDescriptionComponent {
-  private product: Product = {
-    name: '',
-    category: '',
-    info: '',
+  productForm: FormGroup
+  formValidationError: Array<object> = []
+
+  constructor(private formBuilder: FormBuilder) {
+    this.productForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      category: ['', Validators.required],
+      info: '',
+    })
   }
 
-  productForm = this.formBuilder.group(this.product)
+  validate() {
+    const form = this.productForm
 
-  constructor(private formBuilder: FormBuilder) {}
+    function isRequired(fieldName: string) {
+      return form.value[fieldName].length > 0
+    }
+
+    function isInvalid(fieldName: string) {
+      return form.controls[fieldName].invalid
+    }
+
+    return {
+      isRequire: isRequired,
+      isInvalid,
+    }
+  }
 
   handleSubmit() {
-    console.log(this.productForm.value)
+    if (this.productForm.invalid) {
+      for (const [key, value] of Object.entries(this.productForm.controls)) {
+        value.invalid &&
+          this.formValidationError.push({
+            controlName: key,
+            type: value.errors,
+          })
+      }
+      throw new Error(
+        `formValidationErrors: ${JSON.stringify(this.formValidationError)}`
+      )
+    }
+    console.log(this.productForm)
   }
 }
